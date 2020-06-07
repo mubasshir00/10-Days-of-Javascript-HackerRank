e7f82e5570299960350a9a65f3732a4f48bdeec6
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-06-07 09:58:09 +0600

    Update Day 3: Arrays.js

--- Day 3: Arrays.js
+++ Day 3: Arrays.js
@@ -3,3 +3,4 @@ function getSecondLargest(array){
     newArray.sort(function(a,b){return(a-b)}); 
     return newArray[newArray.length-2];
  } 
+//https://www.geeksforgeeks.org/how-to-sort-numeric-array-using-javascript/?ref=rp

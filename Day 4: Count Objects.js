a2d166299e7a17e7fea634bd02732f7ec81a5ae8
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-06-09 22:55:02 +0600

    Create Day 4: Count Objects.js

---
+++ Day 4: Count Objects.js
@@ -0,0 +1,10 @@
+function getCount(objects) {
+    var count = 0;
+    for(let i=0;i<objects.length;i++)
+    {
+        if(objects[i].x === objects[i].y){
+            count++;
+        }
+    }
+    return count ;
+}
